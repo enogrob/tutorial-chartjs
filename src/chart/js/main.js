5a30015568234f4a1545e8738301a4fd9366b8e9
Roberto Nogueira <enogrob@gmail.com>
2017-12-20 15:21:05 -0200

    performed-tutorial

---
+++ src/chart/js/main.js
@@ -0,0 +1,32 @@
+var ctx = document.getElementById('linechart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    // type: 'polarArea',
+    // type: 'line',
+    // type: 'bar',
+    // type: 'radar',
+    // type: 'pie',
+    type: 'doughnut',
+    // type: 'bubble',
+
+    // The data for our dataset
+    data: {
+        labels: ["Strength", "Skill", "Health", "Speed", "Luck"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: ['#f1c40f','#e67e22','#16a085','#2980b9','#8e44ad'],
+            data: [10, 20, 30, 40, 60]
+        }
+      ]
+    },
+
+    // Configuration options go here
+    options: {
+      pieceLabel: {
+        render: 'value',
+        fontSize: 14,
+        fontColor: '#fff',
+        fontStyle: 'bold',
+      }
+    }
+});
